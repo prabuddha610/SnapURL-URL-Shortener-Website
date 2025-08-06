2c5ecea03ab93334b2496c759d3cebf58d8e7dd1
prabuddha610 <prabuddha610@gmail.com>
2025-08-07 00:13:38 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Button } from './components/ui/button';
+
+function App() {
+
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Button>Click</Button>
+    </div>
+  );
+}
+
+export default App
