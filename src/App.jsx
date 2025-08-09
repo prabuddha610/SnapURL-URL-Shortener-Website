1859c4f19ba690e6e98a1daffe8ffcc2a42bd6ce
prabuddha610 <prabuddha610@gmail.com>
2025-08-10 00:48:35 +0530

    Add Protected Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css'
-import { Button } from './components/ui/button';
 import AppLayout from './layouts/app-layout';
 import LandingPage from './pages/landing';
 import Dashboard from './pages/dashboard';
@@ -8,33 +7,42 @@ import Auth from './pages/auth';
 import Link from './pages/link';
 import RedirectLink from './pages/redirect-link';
 import UrlProvider from './context';
+import RequireAuth from './components/require-auth';
 
 const router = createBrowserRouter([
   {
-    element:<AppLayout/>,
-    children:[
+    element: <AppLayout />,
+    children: [
       {
-        path: '/',
-        element: <LandingPage/>
+        path: "/",
+        element: <LandingPage />,
       },
       {
-        path: '/dashboard',
-        element: <Dashboard/>
+        path: "/dashboard",
+        element: (
+          <RequireAuth>
+            <Dashboard />
+          </RequireAuth>
+        ),
       },
       {
-        path: '/auth',
-        element: <Auth/>
+        path: "/auth",
+        element: <Auth />,
       },
       {
-        path: '/link/:id',
-        element: <Link/>
+        path: "/link/:id",
+        element: (
+          <RequireAuth>
+            <Link />
+          </RequireAuth>
+        ),
       },
       {
-        path: '/:id',
-        element: <RedirectLink/>
+        path: "/:id",
+        element: <RedirectLink />,
       },
     ],
-  }
+  },
 ]);
 
 function App() {
