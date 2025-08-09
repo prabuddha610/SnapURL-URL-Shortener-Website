457a71444a0e1d38f3f3780301c9fefb4f7b106a
prabuddha610 <prabuddha610@gmail.com>
2025-08-10 01:39:57 +0530

    Add LinkCard Component

--- src/db/apiClicks.js
+++ src/db/apiClicks.js
@@ -1,11 +1,14 @@
 import supabase from "./supabase";
 
-export async function getClicks() {
-  let {data, error} = await supabase.from("clicks").select("*");
+export async function getClicksForUrls(urlIds) {
+  const { data, error } = await supabase
+    .from("clicks")
+    .select("*")
+    .in("url_id", urlIds);
 
   if (error) {
-    console.error(error);
-    throw new Error("Unable to load Stats");
+    console.error("Error fetching clicks:", error);
+    return null;
   }
 
   return data;
