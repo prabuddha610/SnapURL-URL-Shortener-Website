2d4a4783a58ea6fadc9ed7e4eecf9c44af07fca2
prabuddha610 <prabuddha610@gmail.com>
2025-08-11 23:51:22 +0530

    Add Create URL API

--- src/db/apiUrls.js
+++ src/db/apiUrls.js
@@ -1,4 +1,4 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function getUrls(user_id) {
   let { data, error } = await supabase
@@ -9,6 +9,43 @@ export async function getUrls(user_id) {
   if (error) {
     console.error(error);
     throw new Error("Unable to load URLs");
+  }
+
+  return data;
+}
+
+export async function createUrl(
+  { title, longUrl, customUrl, user_id },
+  qrcode
+) {
+  const short_url = Math.random().toString(36).substr(2, 6);
+  const fileName = `qr-${short_url}`;
+
+  const { error: storageError } = await supabase.storage
+    .from("qrs")
+    .upload(fileName, qrcode);
+
+  if (storageError) throw new Error(storageError.message);
+
+  const qr = `${supabaseUrl}/storage/v1/object/public/qrs/${fileName}`;
+
+  const { data, error } = await supabase
+    .from("urls")
+    .insert([
+      {
+        title,
+        user_id,
+        original_url: longUrl,
+        custom_url: customUrl || null,
+        short_url,
+        qr,
+      },
+    ])
+    .select();
+
+  if (error) {
+    console.error(error);
+    throw new Error("Error creating short URL");
   }
 
   return data;
